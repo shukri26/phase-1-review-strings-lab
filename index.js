16025ce86dfbdaf8b0db9f6872aacdd582c2ce84
Tim <mohamud.hussein@student.moringaschool.com>
2023-06-16 20:58:10 +0300

    first assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+
+let currentUser = "John Doe";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
